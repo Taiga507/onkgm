93f85595dea1c4a769c5a49a9d05d4bbbe54effc
Taiga <taiga507@gmail.com>
2023-05-24 00:28:36 +0300

    v2.0.0

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,19 +1,12 @@
 $(document).ready(function(){
     // Modal
-    // $('[data-modal=consultation]').on('click', function() {
-    //     $('.overlay, #consultation').fadeIn('slow');
-    // });
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
     
-    // $('.modal__close').on('click', function() {
-    //     $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
-    // });
-
-    // $('.button_mini').each(function(i) {
-    //     $(this).on('click', function() {
-    //         $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-    //         $('.overlay, #order').fadeIn('slow');
-    //     })
-    // });
+    $('.modal__close, .modalthanks__white').on('click', function() {
+        $('.overlay, #consultation, #thanks').fadeOut('slow');
+    });
 
     // Validate forms
     function validationForms(form){
@@ -37,13 +30,60 @@ $(document).ready(function(){
             data: $(this).serialize()
         }).done(function() {
             $(this).find("input").val("");
-            $('#consultation, #order').fadeOut();
+            $('#consultation').fadeOut();
             $('.overlay, #thanks').fadeIn('slow');
 
             $('form').trigger('reset');
         });
         return false;
     });
+
+    $('.owl-carousel').owlCarousel({
+        speed:1200,
+        loop:true,
+        margin:50,
+        nav:false,
+        dots:false,
+        autoplay:true,
+        autoplayTimeout:4000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1400:{
+                items:3
+            }
+        }
+    })
+
+
+    $('.zoom-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
 
     new WOW().init();
 });
